46a2999c15b2f7980031b708f66af4d11aa58ab5
himanshu <rackstar17@gmail.com>
2016-01-05 23:00:38 +0530

    server commit

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 var express = require('express');
 var app = express();
+var mongo = require('mongodb').MongoClient;
 
-app.get('*',function(req,res) {
+app.get('/',function(req,res) {
 	res.send('hello world');
+});
+
+
+mongo.connect('mongodb://simpledb:toughpassword@ds037395.mongolab.com:37395/gouravapp', function(err,db) {
+	if(err) throw err;
+	console.log('connected to gouravapp database');
+
+	/*below are the api's made*/
+	app.get('/api/getproducts', function(req,res) {
+		res.send('getproducts api called');
+	});
+
 });
 
 var port = Number(process.env.PORT || 3000);
